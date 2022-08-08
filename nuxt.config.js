742290a5ffb9620f0dc3e5b1fc9ea5520056d59f
Julien Beisel <julienbeisel@gmail.com>
2022-08-08 11:07:03 +0200

    fix: config postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,5 +41,11 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-custom-properties': false,
+      },
+    },
+  },
 }
