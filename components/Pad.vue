f06bdffae0fb22f3ad2477512544477f6b35fc78
Julien Beisel <julienbeisel@gmail.com>
2022-08-31 22:17:23 +0200

    feat: add launch logic

--- components/Pad.vue
+++ components/Pad.vue
@@ -1,15 +1,37 @@
 <template>
   <button
+    @click="emitEvt()"
     class="text-lg m-auto text-lg bg-gray-200 opacity-90 hover:opacity-100 hover:bg-gray-100 rounded-md transition duration-150 ease-in-out"
   >
     <img :src="imgSrc" class="object-fit w-full h-full" />
+    <font-awesome-icon
+      class="col-span-1 h-60 mx-auto"
+      :icon="this.isPlaying()"
+    />
   </button>
 </template>
 
 <script>
 export default {
   name: 'PadMute',
-  props: ['imgSrc'],
+  props: ['imgSrc', 'evtEmit', 'isMuted'],
+  data() {
+    return {
+      mutedClass: 'opacity-10',
+    }
+  },
+  methods: {
+    emitEvt() {
+      this.$nuxt.$emit(this.evtEmit)
+    },
+    isPlaying() {
+      if (this.isMuted) {
+        return 'fa-solid fa-play'
+      } else {
+        return 'fa-solid fa-pause'
+      }
+    },
+  },
 }
 </script>
 
--- components/ToneAbstract.vue
+++ components/ToneAbstract.vue
@@ -2,41 +2,34 @@
   <div>
     <div class="grid grid-cols-12 h-screen max-w-xl m-auto">
       <div class="col-span-8 col-start-3">
-        <div><img src="~/assets/windmill.png" /></div>
-        <div class="grid grid-cols-3">
-          <div class="col-span-1 h-60 mx-6">
-            <ProgressBar :progressValue="100 - highpassPropValue" />
-          </div>
-          <div class="col-span-1 h-60 mx-6">
-            <ProgressBar :progressValue="100 - lowpassPropValue" />
-          </div>
-          <div class="col-span-1 h-60 mx-6">
-            <ProgressBar :progressValue="100 - highpassPropValue" />
-          </div>
+        <div>
+          <img src="~/assets/windmill.png" /> Clique ici avant de tester le
+          reste, ce bouton sera remplacé par la page d'accueil<ButtonLaunch />
         </div>
-        <div class="grid grid-cols-3 w-full mt-6">
-          <font-awesome-icon
-            class="col-span-1 h-60 mx-auto"
-            icon="fa-solid fa-drum"
-          />
-          <font-awesome-icon
-            class="col-span-1 h-60 mx-auto"
-            icon="fa-solid fa-drum"
-          />
-          <font-awesome-icon
-            class="col-span-1 h-60 mx-auto"
-            icon="fa-solid fa-drum"
-          />
-        </div>
+
         <div class="grid grid-cols-3 mt-12 w-full gap-x-10 gap-y-10">
           <Pad
             v-for="item in [
-              require('~/assets/drums.png'),
-              require('~/assets/piano.png'),
-              require('~/assets/bell.png'),
+              [
+                require('~/assets/drums.png'),
+                'launch-drums',
+                this.drumsVolumeValue === -Infinity,
+              ],
+              [
+                require('~/assets/piano.png'),
+                'launch-piano',
+                this.pianoVolumeValue === -Infinity,
+              ],
+              [
+                require('~/assets/bell.png'),
+                'launch-bells',
+                this.bellsVolumeValue === -Infinity,
+              ],
             ]"
-            :key="item"
-            :imgSrc="item"
+            :key="item[0]"
+            :imgSrc="item[0]"
+            :evtEmit="item[1]"
+            :isMuted="item[2]"
             class="col-span-1 mx-auto content-center w-4/5"
           />
         </div>
@@ -49,16 +42,28 @@
 </template>
 
 <script>
-const MAINSONG = 'https://lindecis-windmill.s3.eu-west-3.amazonaws.com/main.mp3'
+const BELLS =
+  'https://lindecis-windmill.s3.eu-west-3.amazonaws.com/1_bells_c.mp3'
+const DRUMS =
+  'https://lindecis-windmill.s3.eu-west-3.amazonaws.com/3_drums_c.mp3'
+
+const PIANO =
+  'https://lindecis-windmill.s3.eu-west-3.amazonaws.com/2_piano_c.mp3'
 export default {
   name: 'ToneAbstract',
   data() {
     return {
       tone: null,
       player: null,
-      analyzer: null,
-      highpass: null,
-      lowpass: null,
+      drums: null,
+      drumsVolume: null,
+      drumsVolumeValue: -Infinity,
+      piano: null,
+      pianoVolume: null,
+      pianoVolumeValue: -Infinity,
+      bells: null,
+      bellsVolume: null,
+      bellsVolumeValue: -Infinity,
     }
   },
   created() {
@@ -67,53 +72,71 @@ export default {
         this.firstStart()
       }
     })
+
+    this.$nuxt.$on('launch-drums', () => {
+      this.drumsVolumeValue = this.switchVolume(this.drumsVolumeValue)
+      this.drumsVolume.set({ volume: this.drumsVolumeValue })
+    })
+
+    this.$nuxt.$on('launch-bells', () => {
+      this.bellsVolumeValue = this.switchVolume(this.bellsVolumeValue)
+      this.bellsVolume.set({ volume: this.bellsVolumeValue })
+    })
+
+    this.$nuxt.$on('launch-piano', () => {
+      this.pianoVolumeValue = this.switchVolume(this.pianoVolumeValue)
+      this.pianoVolume.set({ volume: this.pianoVolumeValue })
+    })
   },
+
   methods: {
+    switchVolume(volume) {
+      if (volume === 0) {
+        return -Infinity
+      }
+      return 0
+    },
     async firstStart() {
       this.tone = await import('tone')
-      this.highpass = new this.tone.Filter(this.highPassValue, 'highpass')
-      this.lowpass = new this.tone.Filter(this.lowpassValue, 'lowpass')
-      this.player = await new this.tone.Player(MAINSONG)
-      this.player.connect(this.highpass)
-      this.highpass.connect(this.lowpass)
-      this.lowpass.connect(this.tone.Master)
 
-      this.player.autostart = true
-      this.player.loop = true
-    },
-  },
-  computed: {
-    highPassValue: {
-      get() {
-        return this.$store.state.highpassValue
-      },
-    },
-    lowpassValue: {
-      get() {
-        return this.$store.state.lowpassValue
-      },
-    },
-    highpassPropValue: {
-      get() {
-        return 100 * this.$store.state.highpassPropValue
-      },
-    },
-    lowpassPropValue: {
-      get() {
-        return 100 * this.$store.state.lowpassPropValue
-      },
-    },
-  },
-  watch: {
-    highPassValue(value) {
-      if (this.highpass !== null) {
-        this.highpass.set({ frequency: value })
-      }
-    },
-    lowpassValue(value) {
-      if (this.lowpass !== null) {
-        this.lowpass.set({ frequency: value })
-      }
+      this.drumsVolume = await new this.tone.Volume(
+        this.drumsVolumeValue
+      ).toDestination()
+      this.pianoVolume = await new this.tone.Volume(
+        this.pianoVolumeValue
+      ).toDestination()
+      this.bellsVolume = await new this.tone.Volume(
+        this.bellsVolumeValue
+      ).toDestination()
+
+      this.drums = await new this.tone.Player({
+        url: DRUMS,
+        loop: true,
+        autostart: true,
+      })
+        .connect(this.drumsVolume)
+        .sync()
+        .start(0)
+
+      this.drums.volume = -Infinity
+      this.piano = await new this.tone.Player({
+        url: PIANO,
+        loop: true,
+        autostart: true,
+      })
+        .connect(this.pianoVolume)
+        .sync()
+        .start(0)
+      this.bells = await new this.tone.Player({
+        url: BELLS,
+        loop: true,
+        autostart: true,
+      })
+        .connect(this.bellsVolume)
+        .sync()
+        .start(0)
+
+      this.tone.Transport.start()
     },
   },
 }
